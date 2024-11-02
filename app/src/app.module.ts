4b03aca0737e76a489e60ea0f0aa518379c9db93
sai-kaneko-31 <sai.kaneko.31@gmail.com>
2024-11-02 17:57:37 +0000

    Generate logs resources

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LogsModule } from './logs/logs.module';
 
 @Module({
-  imports: [],
+  imports: [LogsModule],
   controllers: [AppController],
   providers: [AppService],
 })
